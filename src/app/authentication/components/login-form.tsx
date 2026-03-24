2b164ef732256b2dfcfbc7d4c64207f04d6684f5
Júlio César <juliocesariglesias77@gmail.com>
2026-03-24 16:08:54 -0300

    feat: implement session management and redirection in authentication and dashboard pages

--- src/app/authentication/components/login-form.tsx
+++ src/app/authentication/components/login-form.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
-import z, { email } from "zod";
+import z from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
