49ffb6003e2c4dcc6270ac4b9fd5851f2082f933
Júlio César <juliocesariglesias77@gmail.com>
2026-03-31 15:01:26 -0300

    feat: ensure patient can be deleted

--- src/app/(protected)/patients/_components/table-actions.tsx
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -1,6 +1,21 @@
-import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
-import { useState } from "react";
+"use client";
 
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
+import { useState } from "react";
+import { toast } from "sonner";
+
+import { deletePatient } from "@/actions/delete-patient";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Dialog } from "@/components/ui/dialog";
 import {
@@ -21,6 +36,20 @@ interface PatientsTableActionsProps {
 
 const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
   const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+  const [deleteAlertIsOpen, setDeleteAlertIsOpen] = useState(false);
+
+  const deletePatientAction = useAction(deletePatient, {
+    onSuccess: () => {
+      toast.success("Paciente deletado com sucesso.");
+    },
+    onError: () => {
+      toast.error("Erro ao deletar paciente.");
+    },
+  });
+
+  const handleDeletePatientClick = () => {
+    deletePatientAction.execute({ id: patient.id });
+  };
 
   return (
     <>
@@ -32,14 +61,13 @@ const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            {/* <DropdownMenuLabel>{patient.name}</DropdownMenuLabel> */}
             <DropdownMenuLabel>Ações</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
               <EditIcon className="h-4 w-4" />
               Editar
             </DropdownMenuItem>
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setDeleteAlertIsOpen(true)}>
               <TrashIcon className="h-4 w-4" />
               Deletar
             </DropdownMenuItem>
@@ -52,6 +80,26 @@ const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
           onSuccess={() => setUpsertDialogIsOpen(false)}
         />
       </Dialog>
+
+      <AlertDialog open={deleteAlertIsOpen} onOpenChange={setDeleteAlertIsOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Tem certeza que deseja deletar esse paciente?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Essa ação não pode ser revertida. Isso irá deletar o paciente e
+              todas as consultas agendadas.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction onClick={handleDeletePatientClick}>
+              Deletar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 };
